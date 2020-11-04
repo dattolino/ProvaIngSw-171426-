1e82a4022940de53b684e9e24bc21bf1ce977cf0
datto <datto@LAPTOP-PFU8NVMG>
2020-11-04 11:06:28 +0100

    Initial commit.

---
+++ src/main/java/it/unical/ingsw2020/it/unical/provaIngSw/App.java
@@ -0,0 +1,13 @@
+package it.unical.ingsw2020.it.unical.provaIngSw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
